682968551539f47c69be6d92650f20c1a2621117
grandizzy <grandizzy@localhost>
2020-03-22 20:21:14 +0200

    Add custom gas test

--- test.sh
+++ test.sh
@@ -31,6 +31,13 @@ TEST_RESULT=$?
 docker-compose down
 docker-compose up -d parity-plunger
 sleep 2
+PYTHONPATH=$PYTHONPATH:. py.test --cov=plunger --cov-report=term --cov-append tests/test_custom_gas.py $@
+TEST_RESULT=$?
+
+# Cleanup
+docker-compose down
+docker-compose up -d parity-plunger
+sleep 2
 PYTHONPATH=$PYTHONPATH:. py.test --cov=plunger --cov-report=term --cov-append tests/test_tx_err.py $@
 TEST_RESULT=$?
 
